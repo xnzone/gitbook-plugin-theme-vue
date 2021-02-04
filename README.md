b5fd33bb179dfe06638914af5c6c58d4791542d7
xnzone <ixnzone@foxmail>
2021-02-04 10:36:56 +0800

    auto push

---
+++ README.md
@@ -0,0 +1,9 @@
+gitbook vue theme
+
+```json
+{
+    "plugins":[
+        "theme-vue@git+https://github.com/xnzone/gitbook-plugin-theme-vue.git#0.0.1"
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    book: {
+        assets: "./book",
+        js: [
+        ],
+        css: [
+            "website.css"
+        ],
+        html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
+
+            "head:start": "<!-- head:start -->",
+            "head:end": "<!-- head:end -->",
+
+            "body:start": "<!-- body:start -->",
+            "body:end": "<!-- body:end -->"
+        }
+    },
+    hooks: {
+    }
+};
